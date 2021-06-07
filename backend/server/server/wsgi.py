d8f7fdf57ca4532a8af9de5a661e0dd639b3bf20
Pooran Chandrashekaraiah <pooran.c@fenecon.de>
2021-06-08 00:21:40 +0200

    wsgi

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -14,3 +14,26 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 application = get_wsgi_application()
+
+# ML registry
+import inspect
+from ..apps.ml.registry import  MLRegistry
+
+from ..apps.ml.tweets_classifier.ten_flow import tens_flow_classifier
+
+try:
+    registry = MLRegistry() # create ML registry
+    # Random Forest classifier
+    rf = tens_flow_classifier("../../research/")
+    # add to ML registry
+    registry.add_algorithm(endpoint_name="income_classifier",
+                            algorithm_object=rf,
+                            algorithm_name="random forest",
+                            algorithm_status="production",
+                            algorithm_version="0.0.1",
+                            owner="Piotr",
+                            algorithm_description="rnn",
+                            algorithm_code=inspect.getsource(tens_flow_classifier))
+
+except Exception as e:
+    print("Exception while loading the algorithms to the registry,", str(e))
