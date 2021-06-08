2a46c9fdc6c89f93aafab00b32332539baa52aad
Pooran Chandrashekaraiah <pooran.c@fenecon.de>
2021-06-08 16:46:17 +0200

    Test for predict view

--- backend/server/apps/endpoints/tests.py
+++ backend/server/apps/endpoints/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
 
-# Create your tests here.
+
+
+class EndpointTests(TestCase):
+
+    def test_predict_view(self):
+        client = APIClient()
+        input_data = {
+            "text" : "i am happpy."
+
+        }
+        classifier_url = "/api/v1/Tweets_classifier/predict"
+        response = client.post(classifier_url, input_data, format='json')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data["label"], "joy")
+        self.assertTrue("request_id" in response.data)
+        self.assertTrue("status" in response.data)
